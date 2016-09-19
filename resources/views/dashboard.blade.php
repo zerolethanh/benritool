8b84b0ac4686e37f9343885645ddefb41e3557ea
lvt <zero.lethanh@gmail.com>
2016-09-19 14:12:18 +0900

    add some views and images

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,23 +9,23 @@
                     <div class="panel-heading">Tool List</div>
 
                     <div class="panel-body center-block">
-                        @include('templates.toolicon',['ahref'=>'/money','imgsrc'=>'/imgs/money','caption'=>'Money'])
+{{--                        @include('templates.toolicon',['ahref'=>'/money','imgsrc'=>'/images/money.png','caption'=>'Money'])--}}
 
-                        @include('templates.toolicon',['ahref'=>'/wallet','imgsrc'=>'/imgs/wallet','caption'=>'Wallet'])
+{{--                        @include('templates.toolicon',['ahref'=>'/wallet','imgsrc'=>'/images/wallet.jpg','caption'=>'Wallet'])--}}
 
-                        @include('templates.toolicon',['ahref'=>'/timesheet','imgsrc'=>'/imgs/timesheet','caption'=>'TimeSheets'])
+{{--                        @include('templates.toolicon',['ahref'=>'/timesheet','imgsrc'=>'/images/timesheet.jpg','caption'=>'TimeSheets'])--}}
 
-                        @include('templates.toolicon',['ahref'=>'/cal','imgsrc'=>'/imgs/schedule','caption'=>'Schedule'])
+{{--                        @include('templates.toolicon',['ahref'=>'/cal','imgsrc'=>'/images/schedule.png','caption'=>'Schedule'])--}}
 
-                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/imgs/gpw','caption'=>'Create Password'])
+                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/images/lockandkey.png','caption'=>'Create Password'])
 
-                        @include('templates.toolicon',['ahref'=>'/addr','imgsrc'=>'/imgs/addr','caption'=>'Find Address'])
-                        @include('templates.toolicon',['ahref'=>'/uuid','imgsrc'=>'/imgs/uuid','caption'=>'UUIDs'])
-                        {{--                        @include('templates.toolicon',['ahref'=>'/checkemail','imgsrc'=>'/imgs/checkemail','caption'=>'Check Email'])--}}
+{{--                        @include('templates.toolicon',['ahref'=>'/addr','imgsrc'=>'/images/address.png','caption'=>'Find Address'])--}}
+                        @include('templates.toolicon',['ahref'=>'/uuid','imgsrc'=>'/images/uuid.png','caption'=>'UUIDs'])
+                        {{--                        @include('templates.toolicon',['ahref'=>'/checkemail','imgsrc'=>'/images/checkemail','caption'=>'Check Email'])--}}
 
-                        {{--                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/imgs/gpw','caption'=>'Create Password'])--}}
-                        {{--                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/imgs/gpw','caption'=>'Create Password'])--}}
-                        {{--@include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/imgs/gpw','caption'=>'Create Password'])--}}
+                        {{--                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/images/gpw','caption'=>'Create Password'])--}}
+                        {{--                        @include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/images/gpw','caption'=>'Create Password'])--}}
+                        {{--@include('templates.toolicon',['ahref'=>'/gpw','imgsrc'=>'/images/gpw','caption'=>'Create Password'])--}}
 
                     </div>
                 </div>
